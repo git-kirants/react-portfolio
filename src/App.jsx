d4f5fa6d4505a2a34bbe6aa255045516d205658a
git-kirants <kirants110@gmail.com>
2024-10-31 21:45:50 +0530

    major bug 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import About from "./components/About"
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import kiranLogo from './assets/kiranLogo.png'
+
 
 const App = () => {
-  useEffect(() => {
-    document.querySelector("link[rel='icon']").href = kiranLogo
-  }, [])
-
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
